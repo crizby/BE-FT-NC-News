8d24ef6a1ded942f4213328331fe0d83cce0361c
crizby <chrisburke.cad@gmail.com>
2018-06-28 09:57:58 +0100

    fix to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const app = require('express')();
-const { DB_URL } = process.env.NODE_ENV === 'production' ? process.env.DB_URL : require('./config');
+process.env.NODE_ENV === 'production' ? DB_URL = process.env.DB_URL : { DB_URL } = require('./config');
+
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const apiRouter = require('./routes/api');
